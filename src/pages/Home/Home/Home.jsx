0c4e6d0e26df32f552de4ed52ff45fce9e688152
Mainul Islam <mainulislam6168@gmail.com>
2024-05-11 11:27:43 +0600

    initial project setup with tailwind and react-router-dom

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-neutral-800 font-serif">This is Assignment-12 </h2>
+        </div>
+    );
+};
+
+export default Home;
