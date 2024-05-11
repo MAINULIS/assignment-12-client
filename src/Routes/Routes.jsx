44d30b95c87bf32f864d088a8dc13e39e1edc40b
Mainul Islam <mainulislam6168@gmail.com>
2024-05-11 22:59:25 +0600

    error page implemented

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../pages/Home/Home/Home";
+import ErrorPage from "../components/shared/ErrorPage";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
---
+++ src/components/shared/ErrorPage.jsx
@@ -0,0 +1,29 @@
+import { Link, useRouteError } from "react-router-dom";
+import Lottie from "lottie-react";
+import Container from "./Container";
+import errorImg from '../../assets/animation/404-animation.json'
+
+
+const ErrorPage = () => {
+    const {error} = useRouteError();
+    return (
+        <section className='flex items-center h-screen p-16 bg-gray-100'>
+            <Container>
+            <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+               
+               <Lottie className="md:w-72" animationData={errorImg} loop={true}></Lottie>
+                   <p className="text-2xl font-semibold text-lime-950 md:font-bold md:text-3xl">We Are Sorry, Page Not Found</p>
+                   <p className='lg:text-2xl font-semibold text-red-800 mb-8 mt-2'>
+                       {error?.message}
+                   </p>
+                   <Link to='/' className='btn hover:bg-lime-800 bg-lime-900 text-white'>
+                       Back To Homepage
+                   </Link>
+               
+           </div>
+            </Container>
+        </section>
+    );
+};
+
+export default ErrorPage;
